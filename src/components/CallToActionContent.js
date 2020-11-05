860200bd79fcdf8db807da9bb198443fabbe592a
aaronjan <aaronjan98@gmail.com>
2020-11-05 14:29:03 -0800

    the input container is styled, but the button
    and the image I have to style more

--- src/components/CallToActionContent.js
+++ src/components/CallToActionContent.js
@@ -10,11 +10,11 @@ function CallToActionContent() {
                     <h2>Get your roof inspected remotely and connect with our trusted roofers</h2>
                 </div>
 
-                <div className="input-container" >
+                <form className="input-container" >
                     <img className="home-image" src="https://uploads-ssl.webflow.com/5f127ac8860f158e650f06b5/5f5a6d20aa3c71469c732751_blue-home-pin.png" />
                     <input placeholder="Enter Your Address" />
                     <button>Get Inspection</button>
-                </div>
+                </form>
             </div>
 
             <div className="benefits-bar">
