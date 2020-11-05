dae6b5ef55c914e04c15f40c712bdcd25dd0b920
aaronjan <aaronjan98@gmail.com>
2020-11-05 12:56:51 -0800

    styled first header w/ the correct font, image size
    and padding

--- src/components/CallToActionContent.js
+++ src/components/CallToActionContent.js
@@ -7,22 +7,22 @@ function CallToActionContent() {
             <h1>Free Roof Repair Inspection & Local Roofing Quote</h1>
             <div>Get your roof inspected remotely and connect with our trusted roofers</div>
             <div className="input-container" >
-                <img className="home-image" />
+                <img className="home-image" src="https://uploads-ssl.webflow.com/5f127ac8860f158e650f06b5/5f5a6d20aa3c71469c732751_blue-home-pin.png" />
                 <input placeholder="Enter Your Address" />
                 <button>Get Inspection</button>
             </div>
 
             <div className="benefits-bar">
                 <span className="benefit-1">
-                    <img />
-                    <span>Free Inspection</span>
+                    <img src="https://uploads-ssl.webflow.com/5f127ac8860f158e650f06b5/5f5a67d4bbc9fa4528f259a8_white-satalite.png" width="59" alt="Remote Roofing satalite" className="benefit-1-img benefit-img" />
+                    <span className="free-inspection">Free Inspection</span>
                 </span>
                 <span className="benefit-2">
-                    <img />
+                    <img className="benefit-img" />
                     <span>File A Claim</span>
                 </span>
                 <span className="benefit-3">
-                    <img />
+                    <img className="benefit-img" />
                     <span>Replace or Repair Roof</span>
                 </span>
             </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,10 +7,10 @@ function NavBar() {
     return (
         <>
             <nav className="navbar-container">
-                <img className="logo-image"/>
+                <img className="logo-image" src="https://uploads-ssl.webflow.com/5f127ac8860f158e650f06b5/5f12985a91b23596370f7a81_logo-72-dc-7-f-0-f%402x.png"/>
                 <div className="phone-container">
-                    <img className="phone-icon" />
-                    <span>+1 (800) 401-7716</span>
+                    <img className="phone-icon" src="https://uploads-ssl.webflow.com/5f127ac8860f158e650f06b5/5f1297d1c3fecf36271c10a2_483947%403x.png"/>
+                    <span><strong>+1 (800) 401-7716</strong></span>
                 </div>
             </nav>
             <BottomNav />
